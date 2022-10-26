0892a3043e5f6b33b23a54851ebfdc88e4e0e4bd
gozica <109421350+gozica@users.noreply.github.com>
2022-10-26 16:36:52 -0400

    added weather description

--- src/script.js
+++ src/script.js
@@ -44,10 +44,13 @@ function convertToCelsius(event) {
 function searchCity(response) {
   let h1 = document.querySelector("h1");
   h1.innerHTML = response.data.name;
-
   let temperatureElement = document.querySelector("#temperature");
+let cityElement = document.querySelector("#city");
+let descriptionElement = document.querySelector("#description");
   let windElement = document.querySelector("#wind");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
   windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
